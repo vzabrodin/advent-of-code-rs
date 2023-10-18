cc0190e19330b5ca15bab425031b9326e6bc2fb0
Vitalii Zabrodin <v.d.zabrodin@gmail.com>
2023-10-19 00:38:41 +0300

    refactor: day1

--- src/day1.rs
+++ src/day1.rs
@@ -1,17 +1,17 @@
-use std::{io, fs};
+use std::{fs, io};
 
-fn main() -> io::Result<()> {
-    let content = fs::read_to_string("src/day1.input")?;
-
-    let mut sums: Vec<u32> = content
-        .split("\n\n")
-        .map(|chunk| chunk.lines().flat_map(|x| x.parse::<u32>()).sum::<u32>())
-        .collect();
+pub fn main() -> io::Result<()> {
+    let mut sums = fs::read_to_string("src/day1.input")?
+        .lines()
+        .collect::<Vec<&str>>()
+        .split(|x| x.is_empty())
+        .map(|chunk| chunk.iter().flat_map(|x| x.parse::<u32>()).sum())
+        .collect::<Vec<u32>>();
 
     sums.sort_by(|a, b| b.cmp(a));
 
-    //let result: u32 = sums.into_iter().take(1).sum();
-    let result: u32 = sums.into_iter().take(3).sum();
+    //let result: u32 = sums.iter().take(1).sum();
+    let result: u32 = sums.iter().take(3).sum();
 
     println!("{}", result);
 
